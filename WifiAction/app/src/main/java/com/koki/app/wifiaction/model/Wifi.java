a3eb25ff67272e37e28692b032f30919d49085b5
Nicolas Roos <nicolas.k.roos@gmail.com>
2015-04-29 17:46:43 +0200

    added wifi model

---
+++ WifiAction/app/src/main/java/com/koki/app/wifiaction/model/Wifi.java
@@ -0,0 +1,30 @@
+package com.koki.app.wifiaction.model;
+
+/**
+ * Created by koki on 29/04/15.
+ */
+public class Wifi {
+    private String ssid;
+    private int networkId;
+
+    public Wifi(String ssid, int networkId) {
+        this.ssid = ssid;
+        this.networkId = networkId;
+    }
+
+    public String getSsid() {
+        return ssid;
+    }
+
+    public void setSsid(String ssid) {
+        this.ssid = ssid;
+    }
+
+    public int getNetworkId() {
+        return networkId;
+    }
+
+    public void setNetworkId(int networkId) {
+        this.networkId = networkId;
+    }
+}
