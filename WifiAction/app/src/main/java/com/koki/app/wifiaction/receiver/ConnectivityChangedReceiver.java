88224d5d0414cca0bd86d29ccfb43b9252f952b4
Nicolas Roos <nicolas.k.roos@gmail.com>
2015-05-06 00:07:00 +0200

    keep old wifi and state in sharedPreferences

--- WifiAction/app/src/main/java/com/koki/app/wifiaction/receiver/ConnectivityChangedReceiver.java
+++ WifiAction/app/src/main/java/com/koki/app/wifiaction/receiver/ConnectivityChangedReceiver.java
@@ -3,6 +3,7 @@ package com.koki.app.wifiaction.receiver;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.IBinder;
@@ -13,9 +14,11 @@ import com.koki.app.wifiaction.ActionService;
 public class ConnectivityChangedReceiver extends BroadcastReceiver {
 
     private static final String TAG = "CCReceiver";
+    private static final String CW = "CURRENTWIFI";
+    private static final String CS = "CURRENTSTATE";
 
-    private static String mCurrentWifi = "";
-    private static String mCurrentState = "";
+    //private static String mCurrentWifi = "";
+    //private static String mCurrentState = "";
 
     public ConnectivityChangedReceiver() {
 
@@ -24,21 +27,26 @@ public class ConnectivityChangedReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
+        SharedPreferences sharedPreferences = context.getSharedPreferences("com.koki.app.wifiaction",Context.MODE_PRIVATE);
+        String currentWifi = sharedPreferences.getString(CW,"");
+        String currentState = sharedPreferences.getString(CS,"");
         ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo networkInfo = cm.getActiveNetworkInfo();
         if(networkInfo != null) {
-            if(networkInfo.getTypeName().equals(mCurrentState) && networkInfo.getExtraInfo().equals(mCurrentWifi) ) {
+            if(networkInfo.getTypeName().equals(currentState) && networkInfo.getExtraInfo().equals(currentWifi) ) {
                 //Log.i(TAG,"Already got it");
-            } else if (networkInfo.getTypeName().equals("WIFI") && !networkInfo.getExtraInfo().equals(mCurrentWifi)) {
-                Log.i(TAG,"Changed to new WIFI: " + networkInfo.getExtraInfo());
+            } else if (networkInfo.getTypeName().equals("WIFI") && !networkInfo.getExtraInfo().equals(currentWifi)) {
+                Log.i(TAG, "Changed to new WIFI: " + networkInfo.getExtraInfo());
                 ActionService.startAction(context, networkInfo.getExtraInfo(), true);
-                mCurrentWifi = networkInfo.getExtraInfo();
-                mCurrentState = networkInfo.getTypeName();
-            } else if(mCurrentState.equals("WIFI") && !networkInfo.getTypeName().equals("WIFI")) {
-                Log.i(TAG,"Left WIFI: " + mCurrentWifi);
-                ActionService.startAction(context, mCurrentWifi, false);
-                mCurrentWifi = networkInfo.getExtraInfo();
-                mCurrentState = networkInfo.getTypeName();
+                sharedPreferences.edit().putString(CW, networkInfo.getExtraInfo()).putString(CS,networkInfo.getTypeName()).commit();
+                //mCurrentWifi = networkInfo.getExtraInfo();
+                //mCurrentState = networkInfo.getTypeName();
+            } else if(currentState.equals("WIFI") && !networkInfo.getTypeName().equals("WIFI")) {
+                Log.i(TAG,"Left WIFI: " + currentWifi);
+                ActionService.startAction(context, currentWifi, false);
+                sharedPreferences.edit().putString(CW,networkInfo.getExtraInfo()).putString(CS,networkInfo.getTypeName()).commit();
+                //mCurrentWifi = networkInfo.getExtraInfo();
+                //mCurrentState = networkInfo.getTypeName();
             }
         }
     }
