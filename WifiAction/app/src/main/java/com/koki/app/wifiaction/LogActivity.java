ad07432628cdbb6ee1161127de1511a796390da0
Nicolas Roos <nicolas.k.roos@gmail.com>
2015-05-08 17:23:19 +0200

    LogActivity creates adapter and listview

--- WifiAction/app/src/main/java/com/koki/app/wifiaction/LogActivity.java
+++ WifiAction/app/src/main/java/com/koki/app/wifiaction/LogActivity.java
@@ -4,15 +4,50 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListView;
+
+import com.koki.app.wifiaction.adapter.LogsRealmAdapter;
+import com.koki.app.wifiaction.model.LogEntry;
+
+import io.realm.Realm;
+import io.realm.RealmResults;
 
 
 public class LogActivity extends Activity {
+
+    private ListView lvLogs;
+    private LogsRealmAdapter mLogsRealmAdapter;
+    private Realm realm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log);
+
+        realm = Realm.getInstance(this);
+
+        lvLogs = (ListView) findViewById(R.id.lvLogs);
+        setupLogsListView();
+
     }
 
 
+    private void setupLogsListView() {
+        try {
+            RealmResults<LogEntry> results = realm.where(LogEntry.class).findAll();
+            mLogsRealmAdapter = new LogsRealmAdapter(this,results,true);
+            lvLogs.setAdapter(mLogsRealmAdapter);
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if(realm != null) {
+            realm.close();
+        }
+    }
 }
