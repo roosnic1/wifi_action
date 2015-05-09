6e3eb9bf1a7c563d55d8b490c66ff5ef53010b21
Nicolas Roos <nicolas.k.roos@gmail.com>
2015-05-09 12:36:20 +0200

    removed menu from ActionActivity

--- WifiAction/app/src/main/java/com/koki/app/wifiaction/ActionActivity.java
+++ WifiAction/app/src/main/java/com/koki/app/wifiaction/ActionActivity.java
@@ -7,7 +7,6 @@ import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.ContactsContract;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -96,28 +95,6 @@ public class ActionActivity extends Activity {
         super.onSaveInstanceState(outState);
         outState.putSerializable("WIFIS", wifis);
         outState.putInt("ACTIONTYPE", mActionType);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_notification, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     private  void setupSpActionType() {
